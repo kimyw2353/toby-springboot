2eb8a592dded111674fd9f39cd777331f8be9904
kimyw2353 <kimyw2353@naver.com>
2023-04-12 23:55:30 +0900

    [#3] feat: 의존 오브젝트 추가

--- src/main/java/com/yael/springboot/HelloController.java
+++ src/main/java/com/yael/springboot/HelloController.java
@@ -3,10 +3,14 @@ package com.yael.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(String name) {
-        return "hello" + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/com/yael/springboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.yael.springboot;
+
+public class SimpleHelloService {
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
